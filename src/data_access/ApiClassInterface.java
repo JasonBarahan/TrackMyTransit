625acc33d00278250941907928d69b4d3b34d052
carmen-chau <80921817+carmen-chau@users.noreply.github.com>
2023-11-20 00:10:00 -0500

    added ApiClassInterface.java as an interface that forms structure for all future api classes

---
+++ src/data_access/ApiClassInterface.java
@@ -0,0 +1,8 @@
+package data_access;
+
+public interface ApiClassInterface {
+    // This is an interface defining all the shared attributes and methods that all Api classes should have
+    String PARTIAL_API_URL = null;
+    String API_KEY = null;
+
+}
