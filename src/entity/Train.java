5eee205b84ade94043ea01913b009a8d3f2befe2
JasonBarahan <122300705+JasonBarahan@users.noreply.github.com>
2023-11-21 20:31:47 -0500

    modify presenter, state, view model, output boundary, and output data for visualization use case

--- src/entity/Train.java
+++ src/entity/Train.java
@@ -1,2 +1,58 @@
-package entity;public class Train {
+package entity;
+
+/**
+ * This is temporary and for debugging purposes only.
+ */
+public class Train {
+    // Vehicle ID should never change
+    private final int vehicleID;
+    private double longitude;
+    private double latitude;
+
+    private String routeDestination;
+    private String routeName;
+
+    public Train(int vehicleID, double latitude, double longitude, String routeName, String routeDestination) {
+        this.vehicleID = vehicleID;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.routeDestination = routeDestination;
+        this.routeName = routeName;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public int getVehicleID() {
+        return vehicleID;
+    }
+
+    public String getRouteName() {
+        return routeName;
+    }
+
+    public void setRouteName(String routeName) {
+        this.routeName = routeName;
+    }
+
+    public String getRouteDestination() {
+        return routeDestination;
+    }
+
+    public void setRouteDestination(String routeDestination) {
+        this.routeDestination = routeDestination;
+    }
 }
--- src/interface_adapter/visualize/VisualizePresenter.java
+++ src/interface_adapter/visualize/VisualizePresenter.java
@@ -1,2 +1,56 @@
-package interface_adapter.visualize;public class VisualizePresenter {
+package interface_adapter.visualize;
+
+import interface_adapter.ViewManagerModel;
+import use_case.visualize.VisualizeOutputBoundary;
+import use_case.visualize.VisualizeOutputData;
+
+/**
+ * Prepares the proper view, and relevant data, to present to the user.
+ */
+public class VisualizePresenter implements VisualizeOutputBoundary {
+
+    private final VisualizeViewModel visualizeViewModel;
+    private final ViewManagerModel viewManagerModel;
+
+    /**
+     * Constructor.
+     *
+     * @param visualizeViewModel: the view model for the visualize use case.
+     * @param viewManagerModel: the view manager model.
+     */
+    public VisualizePresenter(VisualizeViewModel visualizeViewModel, ViewManagerModel viewManagerModel) {
+        this.visualizeViewModel = visualizeViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+
+    /**
+     * Prepares the success view for showcase to the user.
+     *
+     * @param data : the output data for the visualize use case to be displayed to the user.
+     */
+    @Override
+    public void prepareSuccessView(VisualizeOutputData data) {
+        // gets the state from the view model
+        VisualizeState visualizeState = visualizeViewModel.getVisualizationState();
+
+        // gets the data
+        visualizeState.setTrains(data.getTrains());
+        // sets the data into the view model's state
+
+        // fire changed property
+
+        // tells the view manager to set the state as the active view
+
+        // fire changed property
+    }
+
+    /**
+     * Prepares the fail view for showcase to the user.
+     *
+     * @param error : the error message.
+     */
+    @Override
+    public void prepareFailView(String error) {
+
+    }
 }
