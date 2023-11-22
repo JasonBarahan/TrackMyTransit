836bf89758919abe6bd81bb31ca3b72f2458bfa0
jacquelyn <jacquelyn.lu@mail.utoronto.ca>
2023-11-22 02:45:03 -0500

    Created TrainFactory

--- src/entity/TrainFactory.java
+++ src/entity/TrainFactory.java
@@ -1,3 +1,13 @@
 package entity;
+
+import java.util.List;
+
 public class TrainFactory {
+    /**
+     * Provide a create method that could "construct" instances of Train without having us to call
+     * the actual Train class constructor.
+     **/
+    public Train create (String name, String trainNumber, String parentLine, float latitude, float longitude, String scheduledTime, String delay) {
+        return new Train (name, trainNumber, parentLine, latitude, longitude, scheduledTime, delay);
+    }
 }
