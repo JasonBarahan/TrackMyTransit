bf3dcc0f65137349255ac61ec6d5dcf196bcfe56
JasonBarahan <122300705+JasonBarahan@users.noreply.github.com>
2023-11-22 12:58:01 -0500

    modify use case interactor and input boundary/data

--- src/use_case/visualize/VisualizeInputBoundary.java
+++ src/use_case/visualize/VisualizeInputBoundary.java
@@ -1,4 +1,14 @@
 package use_case.visualize;
 
+/**
+ * The input data boundary for the Visualize use case.
+ */
 public interface VisualizeInputBoundary {
+
+    /**
+     * Takes the input data and executes the desired actions of the use case.
+     *
+     * @param visualizeInputData: the input data for the visualization use case.
+     */
+    void execute(VisualizeInputData visualizeInputData);
 }
