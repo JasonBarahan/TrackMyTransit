98aee3790858cda64080c97cbd5407d5b92305bd
JasonBarahan <122300705+JasonBarahan@users.noreply.github.com>
2023-11-22 12:58:01 -0500

    modify use case interactor and input boundary/data

--- src/use_case/visualize/VisualizeInteractor.java
+++ src/use_case/visualize/VisualizeInteractor.java
@@ -1,2 +1,40 @@
-package use_case.visualize;public class VisualizeInteractor {
+package use_case.visualize;
+
+import entity.Train;
+
+public class VisualizeInteractor implements VisualizeInputBoundary {
+
+    // This is where a data access object goes. Right now, data is not fetched from any persistent data source,
+    // thus it is not needed.
+
+    // invoke presenter as output boundary
+    final VisualizeOutputBoundary visualizePresenter;
+
+    /**
+     * Constructor.
+     *
+     * @param visualizeOutputBoundary: an output boundary for the Visualize use case.
+     */
+    public VisualizeInteractor(VisualizeOutputBoundary visualizeOutputBoundary) {
+        this.visualizePresenter = visualizeOutputBoundary;
+    }
+
+    /**
+     * Takes the input data and executes the desired actions of the use case.
+     *
+     * @param visualizeInputData : the input data for the visualization use case.
+     */
+    @Override
+    public void execute(VisualizeInputData visualizeInputData) {
+        // get data
+        Train[] trainData = visualizeInputData.getTrains();
+        // TODO: What should the program do to the array of vehicle objects when input data is passed in?
+        // TODO: Usually more code goes here...
+
+        // Generate the output data
+        VisualizeOutputData visualizeOutputData = new VisualizeOutputData(trainData);
+
+        // pass it into the presenter (and through the output boundary)
+        visualizePresenter.prepareSuccessView(visualizeOutputData);
+    }
 }
