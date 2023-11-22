f0dded0d816525a17d1b544450a8ef9a7a0562b6
JasonBarahan <122300705+JasonBarahan@users.noreply.github.com>
2023-11-22 12:58:01 -0500

    modify use case interactor and input boundary/data

--- src/use_case/visualize/VisualizeInputData.java
+++ src/use_case/visualize/VisualizeInputData.java
@@ -1,2 +1,28 @@
-package use_case.visualize;public class VisualizeInputData {
+package use_case.visualize;
+
+import entity.Train;
+
+/**
+ * Input data for the visualization use case. Data is an array of Train objects.
+ */
+public class VisualizeInputData {
+    final private Train[] trains;
+
+    /**
+     * Constructor.
+     *
+     * @param trains: an array of train objects
+     */
+    public VisualizeInputData(Train[] trains) {
+        this.trains = trains;
+    }
+
+    /**
+     * Train data getter.
+     *
+     * @return an array of train objects.
+     */
+    public Train[] getTrains() {
+        return trains;
+    }
 }
