9bf16d22a9d5a1684cd7fa1e27e39626d68abcd6
JasonBarahan <122300705+JasonBarahan@users.noreply.github.com>
2023-11-21 20:31:47 -0500

    modify presenter, state, view model, output boundary, and output data for visualization use case

--- src/interface_adapter/visualize/VisualizeViewModel.java
+++ src/interface_adapter/visualize/VisualizeViewModel.java
@@ -1,2 +1,67 @@
-package interface_adapter.visualize;public class VisualizeViewModel {
+package interface_adapter.visualize;
+
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+/**
+ * The view model for the vehicle visualization use case.
+ */
+public class VisualizeViewModel extends ViewModel {
+
+    // Final labels for internal use
+    private final String WINDOW_LABEL = "TrackMyTransit";
+
+    // User guidance string
+    private final String HELP_LABEL = "Hold the right mouse button while moving it to move the map. Double-click to zoom.";
+
+    // The application state; needed to alert the View on what to present.
+    private VisualizeState visualizationState = new VisualizeState();
+
+    // Add listener support
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    /**
+     * Generates the window name.
+     */
+    public VisualizeViewModel() {
+        super("TrackMyTransit");
+    }
+
+    /**
+     * Fires the property changed call to the View.
+     */
+    @Override
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.visualizationState);
+    }
+
+    /**
+     * Adds a property change listener to the view.
+     *
+     * @param listener: The property change listener.
+     */
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    /**
+     * VisualizationState getter method.
+     *
+     * @return a PropertyChangeListener listener.
+     */
+    public VisualizeState getVisualizationState() {
+        return this.visualizationState;
+    }
+
+    /**
+     * VisualizationState setter method.
+     *
+     * @param visualizationState: the visualizationState to set.
+     */
+    public void setVisualizationState(VisualizeState visualizationState) {
+        this.visualizationState = visualizationState;
+    }
 }
--- src/use_case/visualize/VisualizeOutputData.java
+++ src/use_case/visualize/VisualizeOutputData.java
@@ -1,2 +1,34 @@
-package use_case.visualize;public class VisualizeOutputData {
+package use_case.visualize;
+
+import entity.Train;
+
+public class VisualizeOutputData {
+    private final Train[] trains;
+
+    /*
+        There's no changes to the data passed into this use case. Why bother with input/output data objects?
+
+        In case the implementation of the map visualization changes, particularly with regard to the data required,
+        we can implement these changes in their own use case files.
+
+        Single Responsibility Principle
+     */
+
+    /**
+     * Constructor.
+     *
+     * @param trains: an array of Train objects
+     */
+    public VisualizeOutputData(Train[] trains) {
+        this.trains = trains;
+    }
+
+    /**
+     * Getter for the array of train objects.
+     *
+     * @return the train array Train[]
+     */
+    public Train[] getTrains() {
+        return trains;
+    }
 }
