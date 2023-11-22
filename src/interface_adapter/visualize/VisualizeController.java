f5738494aeeb4052f14823d92e6389acdc4579d0
JasonBarahan <122300705+JasonBarahan@users.noreply.github.com>
2023-11-22 12:57:33 -0500

    modify visualization controller and presenter

--- src/interface_adapter/visualize/VisualizeController.java
+++ src/interface_adapter/visualize/VisualizeController.java
@@ -1,2 +1,34 @@
-package interface_adapter.visualize;public class VisualizeController {
+package interface_adapter.visualize;
+
+import entity.Train;
+import use_case.visualize.VisualizeInputBoundary;
+import use_case.visualize.VisualizeInputData;
+
+/**
+ * The controller for the visualization use case.
+ */
+public class VisualizeController {
+    // get the use case interactor
+    final VisualizeInputBoundary visualizeInteractor;
+
+    /**
+     * Constructor.
+     *
+     * @param visualizeInteractor: the use case interactor for the visualization use case.
+     */
+    public VisualizeController(VisualizeInputBoundary visualizeInteractor) {
+        this.visualizeInteractor = visualizeInteractor;
+    }
+
+    /**
+     * Executes the use case interactor.
+     *
+     * @param trainData: an array of train objects
+     */
+    public void execute(Train[] trainData) {
+        VisualizeInputData visualizeInputData = new VisualizeInputData(trainData);
+
+        // execute
+        visualizeInteractor.execute(visualizeInputData);
+    }
 }
