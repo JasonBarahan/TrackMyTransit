f7381606835f6ddadb73be39977e8d8aa9a24095
arandaaron04 <aaron.aranda@mail.utoronto.ca>
2023-11-27 14:44:02 -0500

    GOTrainsApiClass.java updated.

--- src/data_access/API/GOTrainsApiClass.java
+++ src/data_access/API/GOTrainsApiClass.java
@@ -1,4 +1,59 @@
 package data_access.API;
 
-public class GOTrainsApiClass {
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GOTrainsApiClass implements TrainApiInterface {
+
+    private final String PARTIAL_API_URL = "OpenDataAPI/api/V1";
+    public GOTrainsApiClass () {
+    }
+    public List<String> retrieveTrains(String stationId){
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        HttpUrl httpUrl = new HttpUrl.Builder()
+                .scheme("http")
+                .host("api.openmetrolinx.com")
+                .addPathSegment(PARTIAL_API_URL)
+                .addPathSegment("ServiceataGlance")
+                .addPathSegment("Trains")
+                .addPathSegment("All")
+                // TODO: may need for future
+                // .addPathSegment(stationId) //getting station information for the station with the id denoted by stationId
+                .addQueryParameter("key", API_KEY)
+                .build();
+
+        Request request = new Request.Builder()
+                .url(httpUrl)
+                .addHeader("content-type", "application/json")
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String trainJsonData = response.body().string();
+            JSONObject metaDataJsonObj = new JSONObject(trainJsonData); // This is where the Metadata is located at
+            JSONObject tripsJsonDataObj = metaDataJsonObj.getJSONObject("Trips");
+            JSONArray tripJsonArray = tripsJsonDataObj.getJSONArray("Trip");
+            List<String> tripList = new ArrayList<String>();
+            for (int i = 0; i < tripJsonArray.length(); i++) {
+                JSONObject currTripEntry = tripJsonArray.getJSONObject(i);
+                tripList.add(currTripEntry.getString("id")); // left at "id" for now, to be changed to vehicle
+                //System.out.println(currAmenitiesEntry.getString("Description")); // For debugging purposes
+                System.out.println(httpUrl);
+            }
+            return tripList; // do not print this yet, wait for full change
+
+        } catch (IOException | JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
