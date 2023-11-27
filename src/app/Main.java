a7c3c65aa2bba54556d8f00dbbfaa157d0ee6545
jacquelyn <jacquelyn.lu@mail.utoronto.ca>
2023-11-27 01:23:59 -0500

    added stationinfo related code in Main

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,6 +5,8 @@ import data_access.API.GOStationApiClass;
 import entity.StationFactory;
 import interface_adapter.search.SearchViewModel;
 import interface_adapter.ViewManagerModel;
+import interface_adapter.show_incoming_vehicles.ShowIncomingVehiclesState;
+import interface_adapter.show_incoming_vehicles.ShowIncomingVehiclesViewModel;
 import interface_adapter.station_info.StationInfoViewModel;
 import view.SearchPanelView;
 import view.StationInfoView;
@@ -39,6 +41,8 @@ public class Main {
         // be observed by the Views.
         SearchViewModel searchViewModel = new SearchViewModel();
         StationInfoViewModel stationInfoViewModel = new StationInfoViewModel();
+        ShowIncomingVehiclesViewModel showIncomingVehiclesViewModel = new ShowIncomingVehiclesViewModel();
+
 
         // Creating a DAO called stationDataAccessObject by reading from file revisedStopData.txt, with the creation of the object being done by StationFactory()
         // Note: This process is wrapped in a try-catch block since it is possible that the code throws out an IOException (occurs when the txt file being read does not exist)
@@ -59,7 +63,7 @@ public class Main {
 
         // Creating an instance of StationInfoView. Note: Although this view should have its own use case, for now, since we are NOT displaying other data besides the station name, there is no useCaseFactory for this case
         // This View is only linked to transition from the SearchPanelView (once the other use case are integrated into this view, this will NO LONGER be the case)
-        StationInfoView stationInfoView = new StationInfoView(stationInfoViewModel);
+        StationInfoView stationInfoView = StationInfoUseCaseFactory.create(viewManagerModel, stationInfoViewModel, stationDataAccessObject, showIncomingVehiclesViewModel);
         views.add(stationInfoView, stationInfoView.viewName);
 
         // When initially booting up the application, the stationPanel is the 1st panel displayed to viewers.
