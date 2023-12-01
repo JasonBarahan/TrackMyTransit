8b1901ced11b1445d791567d5f62bf24c9aadc2b
arandaaron04 <aaron.aranda@mail.utoronto.ca>
2023-12-01 02:50:12 -0500

    CoordinatesFunctions.java javaDocs and small changes

--- src/CoordinatesFunctions.java
+++ src/CoordinatesFunctions.java
@@ -3,27 +3,49 @@ public class CoordinatesFunctions {
 
     // private final double GOTRAINSPEED = 38.88; // 140(km/hr) = 38.88(m/s)
 
+    /**
+     * Takes the Longitude and Latitude of two points and solves for distance.
+     *
+     * @param lat1
+     * @param lon1
+     * @param lat2
+     * @param lon2
+     * @return distance
+     */
     static double haversineDistance(double lat1, double lon1,
                                     double lat2, double lon2)
     {
-        // distance between latitudes and longitudes
-        double dLat = Math.toRadians(lat2 - lat1);
-        double dLon = Math.toRadians(lon2 - lon1);
+        /**
+         * The distance between latitudes and longitudes
+         */
+        double distanceLatitude = Math.toRadians(lat2 - lat1);
+        double distanceLongitude = Math.toRadians(lon2 - lon1);
 
-        // convert to radians
+        /**
+         * Convert to Radians
+         */
         lat1 = Math.toRadians(lat1);
         lat2 = Math.toRadians(lat2);
 
-        // apply formulae
-        double a = Math.pow(Math.sin(dLat / 2), 2) +
-                Math.pow(Math.sin(dLon / 2), 2) *
+        /**
+         * Using Haversine to compute distance between the two
+         */
+        double a = Math.pow(Math.sin(distanceLatitude / 2), 2) +
+                Math.pow(Math.sin(distanceLongitude / 2), 2) *
                         Math.cos(lat1) *
                         Math.cos(lat2);
         double rad = 6371;
         double c = 2 * Math.asin(Math.sqrt(a));
-        return rad * c;
+
+        return rad * c; // Distance between the two
     }
 
+    /**
+     * Calculates the time using speed and distance.
+     *
+     * @param distance
+     * @return time
+     */
     static double time(double distance) { // double speed may be changed
         final double GOTRAINSPEED = 38.88;
 
