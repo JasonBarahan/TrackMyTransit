71bf43b817fa83c1b726b69bdc409ba25fc241af
jacquelyn <jacquelyn.lu@mail.utoronto.ca>
2023-12-02 16:48:03 -0500

    modified getIncomingVehicles

--- src/data_access/text_file/FileStationDataAccessObject.java
+++ src/data_access/text_file/FileStationDataAccessObject.java
@@ -100,28 +100,7 @@ public class FileStationDataAccessObject implements SearchDataAccessInterface, S
 
     @Override
     public List<Train> getIncomingVehicles(String inputStationName) {
-        //TODO: Consider moving the "construction" of the train object to a new method called setIncomingVehicles
-        if (incomingVehiclesNotEmpty(inputStationName)) {
-            String stationId = getStationID(inputStationName);
-            List<Train> incomingVehiclesList = new ArrayList<>();
-            for (List<String> vehicles : goVehicleApiClass.retrieveVehicleInfo(stationId)) {
-                String lineName = vehicles.get(0);
-                String lineCode = vehicles.get(1);
-                String trainName = vehicles.get(3);
-                String scheduledTime = vehicles.get(4);
-                String departureTime = vehicles.get(5);
-                String tripNumber = vehicles.get(6);
-                String delay = null;  //TODO: get vehicle delay
-                String latitude = vehicles.get(7);
-                String longitude = vehicles.get(8);
-                Train vehicle = trainFactory.create(lineCode, lineName, trainName, scheduledTime, departureTime,
-                        tripNumber, delay, Float.parseFloat(latitude), Float.parseFloat(longitude));
-                incomingVehiclesList.add(vehicle);
-            }
-//        List<Train> incomingVehiclesList = goVehicleApiClass.retrieveVehicleInfo(stationId);
-            return incomingVehiclesList;
-        }
-        else {return null;}
+        return (stations.get(inputStationName)).getIncomingVehicles();
     }
 
     @Override
