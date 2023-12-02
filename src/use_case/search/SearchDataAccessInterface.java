194cf4e5546d558d29b63ff3b6277aeb8cdce181
carmen-chau <80921817+carmen-chau@users.noreply.github.com>
2023-12-02 00:39:00 -0500

    added methods to DAI based on FileStationDataAccessObject

--- src/use_case/search/SearchDataAccessInterface.java
+++ src/use_case/search/SearchDataAccessInterface.java
@@ -5,19 +5,20 @@ import entity.Station;
 import java.util.List;
 
 public interface SearchDataAccessInterface {
-
-    // new method added to check whether the station whose name the user inputs ACTUALLY exists in the data object
     boolean stationExist(String identifier);
-    boolean validAmenitiesAPICall(String stationName);
+
+    String getAPIMetadataSuccessMessage();
+    String amenitiesAPICallMetadataMessage(String stationName);
 
     String getStationID (String inputStationName);
-    Station getStation(String inputStationName); // attempting to only get proper station name for base implementation of team use case
+    Station getStation(String inputStationName);
 
-    public String getStationName (Station stationObj);
+    String getStationName (Station stationObj);
     String getStationParentLine(String inputStationName);
 
     List<String> getStationAmenities(String inputStationName);
 
-    void setStationAmenities(Station stationObj, List<String> stationObjAmenities);
+    void setStation(Station stationObj);
+    void setStationAmenities(Station stationObj);
 
 }
