dc88a2276048244e804995a3162714e616f05d9d
JasonBarahan <122300705+JasonBarahan@users.noreply.github.com>
2023-12-01 22:11:34 -0500

    add rudimentary tests; yet to be formalized on junit:

--- test/view/MapVisualizationViewTest.java
+++ test/view/MapVisualizationViewTest.java
@@ -6,6 +6,8 @@ import interface_adapter.visualize.VisualizePresenter;
 import interface_adapter.visualize.VisualizeViewModel;
 import use_case.visualize.VisualizeInteractor;
 
+import javax.swing.*;
+import javax.swing.plaf.metal.MetalComboBoxButton;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -48,7 +50,37 @@ public class MapVisualizationViewTest {
         mapVisualizationView.setVisible(true);
 
         // TODO: Check if all mock information aspects are present
+        JRootPane pane = (JRootPane) mapVisualizationView.getComponent(0);
 
+        JPanel panel = (JPanel) pane.getContentPane().getComponent(0);
+        JPanel panel2 = (JPanel) panel.getComponent(0);
+
+        // get the button (no additional components found)
+        JButton button = (JButton) panel2.getComponent(0);
+        System.out.println(button.getComponentCount());
+        // TODO: test if clicking the button changes something
+
+        // get the checkbox (no additional components found)
+        JCheckBox checkBox = (JCheckBox) panel2.getComponent(1);
+        System.out.println(checkBox.getComponentCount());
+        // TODO: test if clicking the checkbox changes something
+
+        // get the combobox
+        JComboBox comboBox = (JComboBox) panel2.getComponent(2);
+        // TODO: more tests
+
+        // check the combobox count matches the list of train objects found
+        assert comboBox.getItemCount() == 2;
+
+        // check the combobox items
+        System.out.println(comboBox.getItemAt(1));
+
+        MetalComboBoxButton metalComboBoxButton = (MetalComboBoxButton) comboBox.getComponent(0);
+        CellRendererPane cellRendererPane = (CellRendererPane) comboBox.getComponent(1);
+        System.out.println(comboBox.getRenderer().toString());
+
+        // check that the last vehicle loaded contains one of the expected outputs
+        assert comboBox.getRenderer().toString().contains("[16:31:00, estimated 16:35:33]  LE - Durham College Oshawa GO");
     }
 
     public static void main(String[] args) {
