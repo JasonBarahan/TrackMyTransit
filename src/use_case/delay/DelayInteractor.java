19b6ebe2ae337ca406e34a52ff17383a965abb5b
arandaaron04 <aaron.aranda@mail.utoronto.ca>
2023-12-03 14:42:42 -0500

    DelayInteractor.java added methods

--- src/use_case/delay/DelayInteractor.java
+++ src/use_case/delay/DelayInteractor.java
@@ -1,4 +1,15 @@
 package use_case.delay;
 
 public interface DelayInteractor {
+    public final String NONE = "No Delay - ";
+    public final String MINIMAL = "Minimal Delay Time - ";
+    public final String DELAY = "Delay - ";
+    public final String MAJOR = "Major Delay - ";
+
+    public final String SECONDS = " second(s)";
+    public final String MINUTES = " minute(s)";
+    public final String HOURS = " hour(s)";
+
+    String delayTime(String scheduled, String computed);
+    double calculated(double time);
 }
