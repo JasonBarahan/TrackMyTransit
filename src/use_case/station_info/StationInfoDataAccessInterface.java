cbe630bb578f565ff91aa9901896104020996327
arandaaron04 <aaron.aranda@mail.utoronto.ca>
2023-12-03 23:13:50 -0500

    StationInfoDataAccessInterface.java merged deleted Delay.java

--- src/use_case/station_info/StationInfoDataAccessInterface.java
+++ src/use_case/station_info/StationInfoDataAccessInterface.java
@@ -9,6 +9,17 @@ import java.util.*;
 //TODO: Review the changes made to getStation, setStation and getIncomingVehicles
 // The changes were made in regard to their method parameters
 public interface StationInfoDataAccessInterface {
+    String NONE = "[No Delay] ";
+    String MINIMAL = "[Minimal Delay] ";
+    String DELAY = "[Delay] ";
+    String MAJOR = "[Major Delay] ";
+
+    String SECONDS = " second(s)";
+    String MINUTES = " minute(s)";
+    String HOURS = " hour(s)";
+
+    String delayTime(String scheduled, String computed) throws ParseException;
+    double calculated(double time);
     boolean incomingVehiclesNotEmpty(String stationName);
 
     Station getStation(String inputStationName);
